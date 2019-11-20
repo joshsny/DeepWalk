b6ab14901ebb83d8ab69a4ac814f6f9aebd9807b
Joshua Snyder <js@joshsny.com>
2019-11-20 16:24:06 +0000

    Addition of new papers from research

--- DeepWalk_python/implementation/deepwalk.py
+++ DeepWalk_python/implementation/deepwalk.py
@@ -1,6 +1,14 @@
 # phi represents the matrix of vector embeddings and W represents a walk
 
-def skipgram( phi, W, window_size ) :
-    for i in len(W):
-        v = W[i]
+
+def skipgram( phi, W, window_size, learning_rate) :
+    for j in len(W):
+        v_j = W[j]
+        for k in [j - window_size, j + window_size]:
+            u_k = W[k]
+            J(phi) = -log(P(u_k, phi(j)))
+            phi = phi - learning_rate * d_phi
     return
+
+def deepwalk( G, window_size, embedding_size, walks_per_vertex, walk_length) :
+
