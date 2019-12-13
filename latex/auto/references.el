328fbf82864aac577223769d94e35a86ed962bb3
Joshua Snyder <js@joshsny.com>
2019-12-13 16:37:01 +0000

    updates to the section on DeepWalk

--- latex/auto/references.el
+++ latex/auto/references.el
@@ -5,6 +5,10 @@
     "deepwalk"
     "mikolov2013efficient"
     "mikolov2013distributed"
-    "levy&goldberg"))
+    "levy&goldberg"
+    "church1990"
+    "qiu2018"
+    "yang2015"
+    "sajjad2019"))
  :bibtex)
 
